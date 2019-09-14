2da18fe114e38024e192da2eaf4669e305723d8e
Siddhant Lad <siddhantnlad@gmail.com>
2019-09-15 02:06:43 +0530

    Design QR code Assigning Left

--- app/src/main/java/com/example/siddhantlad/taqneeq/MyData.java
+++ app/src/main/java/com/example/siddhantlad/taqneeq/MyData.java
@@ -9,6 +9,6 @@ public class MyData {
             R.drawable.chefielogo3, R.drawable.chefielogo3, R.drawable.chefielogo3, R.drawable.chefielogo3,
             R.drawable.chefielogo3, R.drawable.chefielogo3, R.drawable.chefielogo3,R.drawable.chefielogo3};
 
-    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
 }
 
